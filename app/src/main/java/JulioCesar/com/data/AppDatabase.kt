84a1a01192843b6605c654f36d54f9d8b692e3f7
JulioCesarJimenoD <julio_jimeno@ucne.edu.do>
2022-09-24 13:02:43 -0400

    Creado Appmodule Y reparando error en la base de datos

--- app/src/main/java/JulioCesar/com/data/AppDatabase.kt
+++ app/src/main/java/JulioCesar/com/data/AppDatabase.kt
@@ -2,13 +2,14 @@ package JulioCesar.com.data
 
 import JulioCesar.com.data.entities.entity
 import androidx.room.Database
+import androidx.room.RoomDatabase
 
 @Database(
     entities = [entity::class],
     exportSchema = false,
     version = 1
 )
-abstract class AppDatabase {
+abstract class AppDatabase : RoomDatabase(){
     //Los Daos van aqui!!
 
 }
---
+++ app/src/main/java/JulioCesar/com/di/AppModule.kt
@@ -0,0 +1,27 @@
+package JulioCesar.com.di
+
+import JulioCesar.com.data.AppDatabase
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Singleton
+    @Provides
+
+    fun provideDatabase(@ApplicationContext context: Context) : AppDatabase{
+        return Room.databaseBuilder(
+            context,
+            AppDatabase::class.java,"ParcialDb.db"
+        ).fallbackToDestructiveMigration()
+            .build()
+    }
+
+}
