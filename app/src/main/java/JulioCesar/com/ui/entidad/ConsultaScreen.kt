d7aedd408e3b6d181596d33b0844edb941e6bd32
JulioCesarJimenoD <julio_jimeno@ucne.edu.do>
2022-09-26 21:00:33 -0400

    Creando Consulta Y viewModel

---
+++ app/src/main/java/JulioCesar/com/ui/entidad/ConsultaScreen.kt
@@ -0,0 +1,65 @@
+package JulioCesar.com.ui.entidad
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.ViewModel
+import androidx.navigation.NavHostController
+
+@Composable
+fun ConsultaScreen(navHostController: NavHostController, viewModel: EnitdadViewModel = hiltViewModel()){
+
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = { Text(text = "Consulta de Entityes") }
+                )
+            },
+            floatingActionButton = {
+                FloatingActionButton(onClick = { navHostController.navigate("EntityScreen") }) {
+                    Icon(imageVector = Icons.Default.Add, contentDescription = null)
+                }
+            }
+
+        ){
+            Column(
+                modifier = Modifier
+                    .padding(it)
+                    .padding(8.dp)
+            ) {
+                //val lista = viewModel.entityes.collectAsState(initial = emptyList())
+
+                LazyColumn(modifier = Modifier.fillMaxWidth()) {
+                    item {
+                        Row() {
+                            Text(
+                                text = "EntityId"
+                            )
+                            Text(
+                                text = "Nombre"
+                            )
+                        }
+                    }
+//                    items(lista.value) {
+//                        Row() {
+//                            Text(
+//                                text = it.EntityId.toString()
+//                            )
+//                            Text(
+//                                text = it.Nombre
+//                            )
+//                        }
+//                    }
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/JulioCesar/com/ui/entidad/EnitdadViewModel.kt
@@ -0,0 +1,13 @@
+package JulioCesar.com.ui.entidad
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class EnitdadViewModel @Inject constructor(
+    //val entidadRepository: EntidadRepositorio
+) : ViewModel(){
+//    var entites = entidadRepository.getList()
+//    private set
+}
