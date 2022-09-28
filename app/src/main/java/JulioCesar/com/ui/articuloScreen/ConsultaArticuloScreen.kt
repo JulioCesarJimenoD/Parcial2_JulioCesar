990f2deefc8d2bbb99da6d56252ae1b18c141e27
Estudiante General <Julio_jimeno@ucne.edu.do>
2022-09-28 19:45:52 -0400

    Actualizando El ConsultaArticuloScreen

--- app/src/main/java/JulioCesar/com/ui/articuloScreen/ConsultaArticuloScreen.kt
+++ app/src/main/java/JulioCesar/com/ui/articuloScreen/ConsultaArticuloScreen.kt
@@ -1,62 +1,64 @@
 package JulioCesar.com.ui.articuloScreen
 
+import JulioCesar.com.data.articulos.Articulo
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
-
 @Composable
-fun ConsultaScreen(navHostController: NavHostController, viewModel: ArticuloViewModel = hiltViewModel()){
+fun ConsutaArticuloScreen(goToRegistro:() -> Unit, viewModel: ArticuloViewModel = hiltViewModel()) {
+    val ScaffoldState = rememberScaffoldState()
 
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text(text = "Consulta") })
+        },
 
-    Column(modifier = Modifier.fillMaxSize()) {
-        Scaffold(
-            topBar = {
-                TopAppBar(
-                    title = { Text(text = "Consulta de Entityes") }
-                )
-            },
-            floatingActionButton = {
-                FloatingActionButton(onClick = { navHostController.navigate("EntityScreen") }) {
-                    Icon(imageVector = Icons.Default.Add, contentDescription = null)
-                }
-            }
-
-        ){
-            Column(
-                modifier = Modifier
-                    .padding(it)
-                    .padding(8.dp)
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    goToRegistro()
+                },
+                backgroundColor = MaterialTheme.colors.primary
             ) {
-                //val lista = viewModel.entityes.collectAsState(initial = emptyList())
+                Icon(imageVector = Icons.Default.Add, contentDescription = "Nuevo")
+            }
+        },
+        scaffoldState = ScaffoldState
 
-                LazyColumn(modifier = Modifier.fillMaxWidth()) {
-                    item {
-                        Row() {
-                            Text(
-                                text = "EntityId"
-                            )
-                            Text(
-                                text = "Nombre"
-                            )
-                        }
+    ) { it
+        val Listarticulo = viewModel.articulo.collectAsState(initial = emptyList())
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth()){
+
+
+            items(Listarticulo.value){articulo ->
+
+                Column(modifier = Modifier.fillMaxWidth())
+                {
+                    Row(modifier = Modifier
+                        .fillMaxWidth()
+                        .height(30.dp).padding(vertical = 2.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween) {
+                        Text(text = "${articulo.marca}")
+
+                        Text(text = "${articulo.descripcion}",
+                            style = MaterialTheme.typography.h5)
                     }
-//                    items(lista.value) {
-//                        Row() {
-//                            Text(
-//                                text = it.EntityId.toString()
-//                            )
-//                            Text(
-//                                text = it.Nombre
-//                            )
-//                        }
-//                    }
+                    Row( modifier = Modifier.fillMaxWidth().padding(vertical = 1.dp),
+                        horizontalArrangement = Arrangement.Start){
+                        Text(text = "${articulo.existencias}",
+                            style = MaterialTheme.typography.caption)
+                    }
                 }
             }
         }
