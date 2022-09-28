a04ea1caeeb59921d41aad2e787f18d064203ec1
Estudiante General <Julio_jimeno@ucne.edu.do>
2022-09-28 18:18:52 -0400

    Update Articulos

--- app/src/main/java/JulioCesar/com/data/Articulos/articulo.kt
+++ app/src/main/java/JulioCesar/com/data/Articulos/articulo.kt
@@ -3,11 +3,11 @@ package JulioCesar.com.data.Articulos
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "Entidad")
+@Entity(tableName = "Articulo")
 data class articulo(
     @PrimaryKey(autoGenerate = true)
-    val id: Int,
-    val valor1: String,
-    val valor2: String,
-    val valor3: Double
+    val articuloId: Int,
+    val descripcion: String,
+    val marca: String,
+    val existencias: Double
 )
