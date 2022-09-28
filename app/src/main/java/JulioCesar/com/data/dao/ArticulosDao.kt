d1ecdaf1ece8b74df0de9e5c8b45b65c8b192797
Estudiante General <Julio_jimeno@ucne.edu.do>
2022-09-28 18:28:35 -0400

    Creando ArticulosDao

---
+++ app/src/main/java/JulioCesar/com/data/dao/ArticulosDao.kt
@@ -0,0 +1,25 @@
+package JulioCesar.com.data.dao
+
+import JulioCesar.com.data.Articulos.Articulo
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ArticulosDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+
+    suspend fun Insertar(articulo: Articulo)
+
+    @Delete
+    suspend fun Eliminar(articulo: Articulo)
+
+    @Update
+    suspend fun Modificar(articulo: Articulo)
+
+    @Query(
+        "SELECT * FROM Articulos ORDER BY articuloId "
+    )
+    fun getList(): Flow<List<Articulo>>
+
+
+}
