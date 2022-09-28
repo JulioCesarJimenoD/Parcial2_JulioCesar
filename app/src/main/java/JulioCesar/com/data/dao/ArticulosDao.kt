20abb82470e56ba5f508fff2e2dee86f95929c7f
Estudiante General <Julio_jimeno@ucne.edu.do>
2022-09-28 19:04:32 -0400

    Creando el ArticuloRepository

--- app/src/main/java/JulioCesar/com/data/dao/ArticulosDao.kt
+++ app/src/main/java/JulioCesar/com/data/dao/ArticulosDao.kt
@@ -6,6 +6,7 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface ArticulosDao {
+
     @Insert(onConflict = OnConflictStrategy.REPLACE)
 
     suspend fun Insertar(articulo: Articulo)
---
+++ app/src/main/java/JulioCesar/com/data/repository/ArticuloRepository.kt
@@ -0,0 +1,16 @@
+package JulioCesar.com.data.repository
+
+import JulioCesar.com.data.articulos.Articulo
+import JulioCesar.com.data.dao.ArticulosDao
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+
+class ArticuloRepository @Inject constructor(
+    val articulosDao: ArticulosDao
+){
+    suspend fun Insertar( articulo: Articulo) = articulosDao.Insertar(articulo)
+
+    fun Lista(): Flow<List<Articulo>> = articulosDao.getList()
+}
+
