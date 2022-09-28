ad50eacfd30c6cbab08c833ed4807e54f3461241
Estudiante General <Julio_jimeno@ucne.edu.do>
2022-09-28 19:10:43 -0400

    Creando ArticuloViewModel

--- app/src/main/java/JulioCesar/com/ui/articuloScreen/ArticuloViewModel.kt
+++ app/src/main/java/JulioCesar/com/ui/articuloScreen/ArticuloViewModel.kt
@@ -1,13 +1,40 @@
 package JulioCesar.com.ui.articuloScreen
 
+import JulioCesar.com.data.articulos.Articulo
+import JulioCesar.com.data.repository.ArticuloRepository
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
+
 
 @HiltViewModel
 class ArticuloViewModel @Inject constructor(
-    //val entidadRepository: EntidadRepositorio
-) : ViewModel(){
-//    var entites = entidadRepository.getList()
-//    private set
+    val articuloRepository: ArticuloRepository
+):ViewModel(){
+    var marca by mutableStateOf("")
+    var descripcion by mutableStateOf("")
+    var existencias by mutableStateOf(0.0)
+
+    var articulo = articuloRepository.getList()
+        private set
+
+
+    fun Guardar(){
+        viewModelScope.launch {
+            articuloRepository.Insertar(
+                Articulo(
+                    articuloId = 0,
+                    descripcion = "",
+                    marca = "",
+                    existencias = 0.0
+            )
+            )
+        }
+    }
 }
+
